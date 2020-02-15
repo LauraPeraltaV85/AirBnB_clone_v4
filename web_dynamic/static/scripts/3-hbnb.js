d7ba605d8e7e70df3d7fcef418ed79f0aea3f1a8
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-02-15 19:43:44 +0000

    [feature] fetch places files

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+  const checkboxArray = Array.from(checkboxes);
+  const checkList = [];
+
+  function removeItem (array, item) {
+    for (var i in array) {
+      if (array[i] === item) {
+        array.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  function confirmCheck () {
+    if (this.checked) {
+      checkList.push(this.getAttribute('data-name'));
+    } else {
+      removeItem(checkList, this.getAttribute('data-name'));
+    }
+    $('.amenities h4').text(checkList.sort());
+  }
+
+  checkboxArray.forEach(function (checkbox) {
+    checkbox.addEventListener('change', confirmCheck);
+  });
+
+  $.ajax({url: "http://0.0.0.0:5001/api/v1/status/"})
+  .done(function(data){
+    if (data.status == 'OK') {
+      $('div#api_status').addClass('available');
+    }
+  })
+  .fail(function(jqXHR, textStatus){
+    $('div#api_status').removeClass('available');
+  });
+
+  /*
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, textStatus)
+	{
+    if (data.status == 'OK') {
+      $('div#api_status').addClass('available');
+    }
+	});
+  */
+  $.ajax({
+      url: "http://0.0.0.0:5001/api/v1/places_search/",
+      type: POST,
+      data: {},
+      contentType: "application/json"
+  });
+    for (let i = 0; i < data.length; i++) {
+	$('section.places').append($('<article></article>').text(data.place[i]));
+});
