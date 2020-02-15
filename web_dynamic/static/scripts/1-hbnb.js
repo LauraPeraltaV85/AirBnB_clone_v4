83d46b97a41dfdd1392def697f1e929ba190e207
julgachancipa <julianaga01@gmail.com>
2020-02-14 19:23:37 -0500

    [FEATURE] Select some Amenities to be comfortable

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,27 @@
-$( document ).ready(function() {
-    $('.amenities.popover li').click(function() {
-	if ($("input[type=checkbox]").prop(
-            ":checked")) {
-            alert("Check box in Checked");
-        } else {
-            alert("Check box is Unchecked");
-        }
-    })
-})
+$(document).ready(function () {
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+  const checkboxArray = Array.from(checkboxes);
+  const checkList = [];
+
+  function removeItem (array, item) {
+    for (var i in array) {
+      if (array[i] === item) {
+        array.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  function confirmCheck () {
+    if (this.checked) {
+      checkList.push(this.getAttribute('data-name'));
+    } else {
+      removeItem(checkList, this.getAttribute('data-name'));
+    }
+    $('.amenities h4').text(checkList.sort());
+  }
+
+  checkboxArray.forEach(function (checkbox) {
+    checkbox.addEventListener('change', confirmCheck);
+  });
+});
